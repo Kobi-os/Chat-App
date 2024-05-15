c60099795ae06ba17838c1123cc9f38f05102c8f
Wojtek <wojtek.kobier@wp.pl>
2024-05-15 23:54:02 +0200

    backedn

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -2,7 +2,9 @@ import {MultiChatSocket, MultiChatWindow, useMultiChatLogic} from "react-chat-en
 
 const ChatsPage = (props) => {
     const chatProps = useMultiChatLogic('c72fb7bc-1595-4fb6-b49b-6d327bb0b57c', 
+    // eslint-disable-next-line react/prop-types
     props.user.username, 
+    // eslint-disable-next-line react/prop-types
     props.user.secret);
     return <div style={{ height: '100vh' }}>
         <MultiChatSocket {...chatProps} />
